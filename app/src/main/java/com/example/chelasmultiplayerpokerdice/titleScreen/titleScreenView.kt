a3a6e7d2e1a20816bd5aeda8c0575d4f0d4b9606
Diogo Arnauth Fernandes Leitão <dioarnauth@gmail.com>
2025-09-13 12:40:25 +0100

    aboutScreen inicio

--- app/src/main/java/com/example/chelasmultiplayerpokerdice/titleScreen/titleScreenView.kt
+++ app/src/main/java/com/example/chelasmultiplayerpokerdice/titleScreen/titleScreenView.kt
@@ -6,14 +6,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -112,15 +115,27 @@ fun TitleScreenView(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GenericTopAppBar(title: String, actions: @Composable () -> Unit) {
-    TODO("Not yet implemented")
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Start
+            )
+        },
+        actions = {
+            actions()
+        }
+    )
 }
 
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun HomeViewPreview() {
+fun TitleScreenView() {
     TitleScreenView(
         creators = listOf(
             "Diogo Arnauth",
