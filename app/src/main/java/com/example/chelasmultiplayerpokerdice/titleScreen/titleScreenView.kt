6e8d0538c175d01f2a219548461c54fb2c6249b8
RenataCastanheira <renatacastanheira.atb23@gmail.com>
2025-09-13 12:25:39 +0100

    titleScreen-cont

---
+++ app/src/main/java/com/example/chelasmultiplayerpokerdice/titleScreen/titleScreenView.kt
@@ -0,0 +1,134 @@
+package com.example.chimp.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+const val TITLESCREEN_VIEW_TAG = "Title View"
+const val TITLESCREEN_STARTMATCH_BUTTON = "Start match button on title screen"
+const val TITLESCREEN_PROFILE_BUTTON = "Profile button on title screen"
+const val TITLESCREEN_ABOUT_BUTTON = "About button on title screen"
+
+const val TITLESCREEN_MADE_BY_TEXT = "made by text on Title screen"
+
+@Composable
+fun TitleScreenView(
+    creators: List<String>,
+    startMatchFunction: () -> Unit,
+    profileFunction: () -> Unit,
+    aboutFunction: () -> Unit
+) {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .testTag(TITLESCREEN_VIEW_TAG),
+        topBar = {
+            GenericTopAppBar(
+                title = "Chelas Multi-player Poker Dice",
+                actions = {
+                    TextButton(
+                        onClick = { startMatchFunction() },
+                        modifier = Modifier.testTag(TITLESCREEN_STARTMATCH_BUTTON)
+                    ) {
+                        Text(
+                            text = "start match",
+                            color = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                    TextButton(
+                        onClick = { profileFunction() },
+                        modifier = Modifier.testTag(TITLESCREEN_PROFILE_BUTTON)
+                    ) {
+                        Text(
+                            text = "profile",
+                            color = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                    TextButton(
+                        onClick = { aboutFunction() },
+                        modifier = Modifier.testTag(TITLESCREEN_ABOUT_BUTTON)
+                    ) {
+                        Text(
+                            text = "about",
+                            color = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier.weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "welcome",
+                    style = MaterialTheme.typography.titleSmall,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    modifier = Modifier.testTag(TITLESCREEN_MADE_BY_TEXT),
+                    text = "creators",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+                creators.forEach {
+                    Text(
+                        text = it,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GenericTopAppBar(title: String, actions: @Composable () -> Unit) {
+    TODO("Not yet implemented")
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun HomeViewPreview() {
+    TitleScreenView(
+        creators = listOf(
+            "Diogo Arnauth",
+            "Humberto Carvalho",
+            "Renata Castanheira",
+        ),
+        startMatchFunction = { },
+        profileFunction = { },
+        aboutFunction = { }
+    )
+}
