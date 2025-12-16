baf801091aa0981aace9ccc2963d6b732b83de3e
humberto_carv <a50500@alunos.isel.pt>
2025-12-16 22:39:26 +0000

    + GameViewModel.kt

--- app/src/main/java/com/example/chelasmultiplayerpokerdice/game/GameViewModel.kt
+++ app/src/main/java/com/example/chelasmultiplayerpokerdice/game/GameViewModel.kt
@@ -1,42 +1,55 @@
 package com.example.chelasmultiplayerpokerdice.game
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
-class GameViewModel(private val service: GameService) : ViewModel() {
+class GameViewModel(private val repository: GameRepository) : ViewModel() {
 
     private val _state = MutableStateFlow<GameScreenState>(GameScreenState.Loading)
     val state: StateFlow<GameScreenState> = _state.asStateFlow()
 
-    fun loadGame(lobbyId: Int, token: String) {
-        if (_state.value is GameScreenState.Playing) return
+    init {
+
         viewModelScope.launch {
-            try {
-                _state.value = GameScreenState.Loading
-                val initialState = service.getInitialGameState(lobbyId, token)
-                _state.value = GameScreenState.Playing(initialState)
-            } catch (e: Throwable) {
-                _state.value = GameScreenState.Error(e.message ?: "Erro ao carregar jogo")
+            repository.gameState.collectLatest { newGameState ->
+                if (newGameState == null) {
+                    _state.value = GameScreenState.Loading
+                } else {
+                    val isRoundOver = newGameState.players.all { it.dice != null }
+                    val isGameOver = newGameState.finalWinners.isNotEmpty()
+
+                    _state.value = when {
+                        isGameOver -> GameScreenState.GameOver(
+                            newGameState,
+                            newGameState.finalWinners
+                        )
+
+                        isRoundOver -> {
+                            val winner = newGameState.players.maxByOrNull { it.hand?.score ?: 0.0 }
+                                ?: newGameState.players.first()
+                            GameScreenState.RoundOver(newGameState, winner)
+                        }
+
+                        else -> GameScreenState.Playing(newGameState)
+                    }
+                }
             }
         }
     }
 
-
-    fun onDieClicked(dieId: Int) {
-        val currentState = _state.value
-        if (currentState is GameScreenState.Playing) {
-            val newDice = currentState.gameState.dice.map {
-                if (it.id == dieId) it.copy(isHeld = !it.isHeld) else it
+    fun loadGame(lobbyId: Int, token: String) {
+        viewModelScope.launch {
+            try {
+                repository.fetchGame(lobbyId, token)
+            } catch (e: Throwable) {
+                _state.value = GameScreenState.Error(e.message ?: "Erro ao carregar jogo")
             }
-            _state.value = GameScreenState.Playing(
-                currentState.gameState.copy(dice = newDice)
-            )
         }
     }
 
@@ -45,73 +58,51 @@ class GameViewModel(private val service: GameService) : ViewModel() {
         if (currentState is GameScreenState.Playing && currentState.gameState.canRoll) {
             viewModelScope.launch {
                 try {
-                    val newState = service.rollDice(currentState.gameState, token)
-                    _state.value = GameScreenState.Playing(newState)
+                    repository.rollDice(token)
                 } catch (e: Throwable) {
-                    _state.value = GameScreenState.Error(e.message ?: "Erro ao rolar")
+                    _state.value = GameScreenState.Error("Erro ao rodar os dados")
                 }
             }
         }
     }
-
 
     fun onEndTurnClicked(token: String) {
-        val currentState = _state.value
-        if (currentState is GameScreenState.Playing) {
-            viewModelScope.launch {
-                try {
-                    _state.value =
-                        GameScreenState.Loading // TODO("MUDAR PARA A PREPARAR TURNO) adicionar param ao loading ?
-
-                    val newState = service.endTurnAndSimulate(currentState.gameState, token)
-
-                    val allPlayersHavePlayed = newState.players.all { it.dice != null }
-
-                    if (allPlayersHavePlayed) {
-
-                        val roundWinner = newState.players.maxBy { it.hand!!.score }
-                        _state.value = GameScreenState.RoundOver(newState, roundWinner)
-
-                    } else _state.value = GameScreenState.Playing(newState)
-
-                } catch (e: Throwable) {
-                    _state.value = GameScreenState.Error(e.message ?: "Erro ao terminar turno")
-                }
+        viewModelScope.launch {
+            try {
+                _state.value = GameScreenState.Loading
+                repository.endTurn(token)
+            } catch (e: Throwable) {
+                _state.value = GameScreenState.Error("Erro ao terminar turno")
             }
         }
     }
 
-
     fun onStartNextRound() {
-        val currentState = _state.value
-
-        if (currentState is GameScreenState.RoundOver) {
-            viewModelScope.launch {
-                try {
-                    _state.value = GameScreenState.Loading // TODO("MUDAR PARA A PREPARAR ROUND")
-
-                    val nextRoundState = service.startNextRound(currentState.gameState)
-
-                    if (nextRoundState.finalWinners.isNotEmpty()) {
-
-                        _state.value = GameScreenState.GameOver(
-                            nextRoundState, nextRoundState.finalWinners
-                        )
-                    } else {
-                        _state.value = GameScreenState.Playing(nextRoundState)
-                    }
-                } catch (e: Throwable) {
-                    _state.value = GameScreenState.Error(e.message ?: "Erro ao iniciar ronda")
-                }
+        viewModelScope.launch {
+            try {
+                repository.startNextRound()
+            } catch (e: Throwable) {
+                _state.value = GameScreenState.Error("Erro ao iniciar ronda")
             }
+        }
+    }
+
+    fun onDieClicked(dieId: Int) {
+        val currentState = _state.value
+        if (currentState is GameScreenState.Playing) {
+            val newDice = currentState.gameState.dice.map {
+                if (it.id == dieId) it.copy(isHeld = !it.isHeld) else it
+            }
+            _state.value = GameScreenState.Playing(currentState.gameState.copy(dice = newDice))
         }
     }
 }
 
+// TODO ("REROLL DOS LOBBIES DEIXOU DE FUNCIONAR DEVIDAMENTE")
+
 @Suppress("UNCHECKED_CAST")
-class GameViewModelFactory(private val service: GameService):
-    ViewModelProvider.Factory {
+class GameViewModelFactory(private val repository: GameRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return GameViewModel(service) as T
+        return GameViewModel(repository) as T
     }
 }
