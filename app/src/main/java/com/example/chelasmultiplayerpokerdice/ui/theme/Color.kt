cd5e426bd9c3e1f080949c814cf1a8343204d9ca
RenataCastanheira <renatacastanheira.atb23@gmail.com>
2025-12-22 00:04:40 +0000

    -melhorias no compose

--- app/src/main/java/com/example/chelasmultiplayerpokerdice/ui/theme/Color.kt
+++ app/src/main/java/com/example/chelasmultiplayerpokerdice/ui/theme/Color.kt
@@ -2,12 +2,12 @@ package com.example.chelasmultiplayerpokerdice.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFFA65B5B)
+val PurpleGrey80 = Color(0xFF341717)
+val Pink80 = Color(0xFFEFB8B8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val Purple40 = Color(0xFFA45050)
+val PurpleGrey40 = Color(0xFF715B5B)
 val Pink40 = Color(0xFF7D5260)
 
 val pokerGreen = Color(0xFF6AE5C0)
